1254c167f188926b25dbacc616c492396fbcbe86
Andrew Christensen <andrew-christensen@uiowa.edu>
2026-05-03 18:03:23 -0500

    Added description of  and  parameters

--- src/models/svm.py
+++ src/models/svm.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class SVM:
 
-    """Linear Support Vector Machine
+    """Linear Multi-Class Support Vector Machine
     
 
     Parameters
@@ -11,6 +11,17 @@ class SVM:
         Regularization parameter. The strength of the regularization
         controls how strict the training examples must be correctly
         classified. Value must be strictly positive.
+
+    rho : float, default=1
+        Augmented Lagrangian parameter used in the ADMM implementation
+        of the multi-class SVM.
+
+        Larger values of `rho` enforce stricter adherence to the 
+        primal equality constraints, but can hinder the convergence
+        of the dual updates.
+
+    max_iter : int, default=1000
+        Maximum number of ADMM iterations
     
     """
 
@@ -21,3 +32,4 @@ class SVM:
         self.max_iter       = max_iter
 
         assert self.C > 0, 'Regularization parameter must be strictly positive'
+        assert self.rho > 0, 'Augmented Lagrangian penalty parameter must be strictly positive'
