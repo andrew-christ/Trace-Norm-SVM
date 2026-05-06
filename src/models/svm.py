fe34d65a208d283b1bac28fb01dd502c37dca9a0
Andrew Christensen <andrew-christensen@uiowa.edu>
2026-05-05 22:29:16 -0500

    Added predict function to svm

--- src/models/svm.py
+++ src/models/svm.py
@@ -155,6 +155,41 @@ class SVM:
             self.U2 += self.rho * (self.S - (X @ self.W + self.b))
 
         return self
+    
+    def predict(self, X, return_scores=False):
+        """
+        Compute predictions using multi-class linear SVM.
+
+        Class scores are of the form:
+
+            S = XW + b
+
+        where each entry S_{i,k} represents the score for
+        class k for sample i.
+
+        Parameters
+        ----------
+        X : ndarray of shape (n_samples, n_features)
+            Training data matrix.
+
+        return_scores : bool, default=False
+            If True, returns raw class scores instead of predicted labels
+
+        Returns
+        -------
+        ndarray
+            If return_scores=True:
+                of shape (n_samples, n_classes)
+
+            Otherwise:
+                of shape (n_samples,)
+
+        """
+
+        if return_scores:
+            return X @ self.W + self.b
+        
+        return np.argmax(X @ self.W + self.b, axis=1)
 
     def dual_simplex_projection(self, V):
         """
