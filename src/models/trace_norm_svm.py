5e9033012056ea2c639bce39001bf996c900c5fa
Andrew Christensen <andrew-christensen@uiowa.edu>
2026-05-12 15:37:22 -0500

    Added nuclear norm proximal operator

--- src/models/trace_norm_svm.py
+++ src/models/trace_norm_svm.py
@@ -40,3 +40,56 @@ class TraceNormSVM:
 
         assert self.C > 0, 'Regularization parameter must be strictly positive'
         assert self.rho > 0, 'Augmented Lagrangian penalty parameter must be strictly positive'
+
+
+
+
+
+    def nuclear_prox(self, Z):
+        """
+        Proximal operator of the nuclear norm via singular value
+        thresholding (SVT).
+
+        Solves the optimization problem:
+
+            minimize_X  ||X||_* + (rho / 2) * ||X - Z||_F^2
+
+        where:
+            - ||⋅||_* is the nuclear norm (sum of singular values)
+            - ||⋅||_F is the Frobenius norm
+            - rho > 0 is the regularization parameter
+
+        This has a closed-form solution given by the 
+        soft-thresholding of the singular values of Z.
+
+        Parameters
+        ----------
+        Z : ndarray of shape (n_features, n_classes)
+            Input matrix.
+
+        Returns
+        -------
+        X : ndarray of shape (n_features, n_classes)
+            Solution of proximal problem
+
+        Notes
+        -----
+        This is also known as Singular Value Thresholding (SVT) [1],
+        commonly used in matrix completion and low-rank regularization.
+
+        References
+        ----------
+            [1] Cai, Jian-Feng, Emmanuel J. Candès, and Zuowei Shen. 
+                "A singular value thresholding algorithm for matrix completion." 
+                SIAM Journal on optimization 20.4 (2010): 1956-1982
+
+        """
+
+        # Compute the compact singular value decomposition (SVD)
+        U, s, Vt = np.linalg.svd(Z, full_matrices=False)
+
+        # Apply soft-threholding to the singular values.
+        s_thresh = np.maximum(s - 1.0 / self.rho, 0)
+
+        # Reconstruct the matrix using the thresholded singular values
+        return U @ np.diag(s_thresh) @ Vt
