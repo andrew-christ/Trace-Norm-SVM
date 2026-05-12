8080b3779c4132f46287be03048ac41b616df77e
Andrew Christensen <andrew-christensen@uiowa.edu>
2026-05-12 14:53:18 -0500

    Updated trace norm svm init function

---
+++ src/models/trace_norm_svm.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+from sklearn.preprocessing import LabelEncoder
+
+from tqdm import tqdm
+
+
+class TraceNormSVM:
+
+    """
+    Linear Multi-Class Support Vector Machine 
+    with Trace Norm Regularization
+
+
+    Parameters
+    ----------
+    C : float, default=0.1
+        Regularization parameter. The strength of the regularization 
+        controls how strict the training examples must be correctly 
+        classified. Value must be strictly positive.
+
+    rho : float, default=1
+        Augmented Lagrangian parameter used in the ADMM implementation
+        of the multi-class SVM.
+
+        Larger values of `rho` enforce stricter adherence to the 
+        primal equality constraints, but can hinder the convergence 
+        of the dual updates.
+
+    max_iter : int, default=1000
+        Maximum number of ADMM iterations
+    
+    """
+
+    def __init__(self, C=0.1, rho=1, max_iter=1000):
+
+        self.C          = C
+        self.rho        = rho
+        self.max_iter   = int(max_iter)
+
+        assert self.C > 0, 'Regularization parameter must be strictly positive'
+        assert self.rho > 0, 'Augmented Lagrangian penalty parameter must be strictly positive'
